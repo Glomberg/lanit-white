035630d6f5f8d4f67c841ca1b94a4d0e3e7e4248
Glomberg <Bazz@bk.ru>
2015-03-26 17:40:54 +0400

    5

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,18 @@
 ﻿$(document).ready(function(){
+	//parallax
+	if (navigator.platform == 'iPad' || navigator.platform == 'iPhone' || navigator.platform == 'iPod') {
+		$("header").css("background-position","50% 0");
+	} else {
+		$(document).bind('scroll', function() {
+			var scroll = $(document).scrollTop();
+			if (scroll <= 250) {
+				var yPos = scroll * 0.5;
+				var coords = '50% -'+ yPos + 'px';
+				$("header").css("background-position",coords);
+			}
+		});
+	}
+	//scroller
 	$(".slider-inner").jScrollPane({
 		contentWidth: '2106'		
 	});
